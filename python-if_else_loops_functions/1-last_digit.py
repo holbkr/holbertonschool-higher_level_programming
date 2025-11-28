f9c3911de77fd9298d11e2f028facc9f45efec55
holbkr <elementkripton@gmail.com>
2025-11-28 19:11:13 +0400

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
 last_digit=abs(number)%10
 if number<0:
     last_digit = last_digit*-1
@@ -12,6 +11,5 @@ elif last_digit ==0:
     msg="and is 0"
 else:
     msg="and is less than 6 and not 0"
-
-
 print(f"Last digit of {number} is {last_digit} {msg}")
+
