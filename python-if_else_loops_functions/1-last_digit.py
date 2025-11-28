f36cbfaf41d23a1affda4bbe2b7c5f16df3f493e
holbkr <elementkripton@gmail.com>
2025-11-28 19:13:43 +0400

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,10 @@ last_digit = abs(number) % 10
 if number < 0:
     last_digit = last_digit*-1
 if last_digit > 5:
-    msg="and is greater than 5"
+    msg = "and is greater than 5"
 elif last_digit == 0:
-    msg="and is 0"
+    msg = "and is 0"
 else:
-    msg="and is less than 6 and not 0"
+    msg = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {last_digit} {msg}")
+
