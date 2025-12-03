c2578f905d6a29f74d3ad8862a2bb5efa6916f19
holbkr <elementkripton@gmail.com>
2025-12-03 14:41:26 +0400

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return ("None")
+        return None
     x = my_list[0]
     for i in my_list:
         if i > x:
             x = i
-    return (x)
+    return x
   
