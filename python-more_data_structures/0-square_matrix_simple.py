322d1c61300a5a2a5b32d88d0d4d08694908e185
E <emilakbarov857@gmail.com>
2025-12-08 14:15:37 +0400

    dfsd

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    matrix2 = matrix[:]
-    for i in matrix2:
-        for j in i:
-            j=j**2
+    matrix2 = [row[:] for row in matrix]
+    for i in range(len(matrix2)):
+        for j in range(len(matrix2[i])):
+            matrix2[i][j] = matrix2[i][j] ** 2
     return matrix2
