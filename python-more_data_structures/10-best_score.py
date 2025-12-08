a42c69fb2e0c025837896e745dda44103be7b9c2
E <emilakbarov857@gmail.com>
2025-12-08 15:07:02 +0400

    dfsd

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,14 +3,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
     bk = None
     mv = -float('inf')
-
     for key, value in a_dictionary.items():
-        
         if value > mv:
             mv = value
             bk = key
-            
     return bk
