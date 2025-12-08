aacd46479a9b02433820a4ee917e3600792846e0
E <emilakbarov857@gmail.com>
2025-12-08 15:13:36 +0400

    dfsd

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    return {k: v * 2 for k, v in a_dictionary.items()}
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
