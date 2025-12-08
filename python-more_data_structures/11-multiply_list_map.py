964efefc0c642d4a2356364e3bfa62ad18908530
E <emilakbarov857@gmail.com>
2025-12-08 15:11:33 +0400

    dfsd

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    nl = []
-    for i in my_list:
-        nl += [i*number]
-    return nl
+    return list(map(lambda x: x * number, my_list))
