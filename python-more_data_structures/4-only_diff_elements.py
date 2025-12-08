9d4e3d0c9e65ce17677b975216f67f8d7bb53fc6
E <emilakbarov857@gmail.com>
2025-12-08 14:27:40 +0400

    dfsd

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,8 +4,8 @@ def only_diff_elements(set_1, set_2):
     common = []
     for i in set_1:
         if i not in set_2:
-            common+=[i]
+            common += [i]
     for i in set_2:
         if i not in set_1 and i not in common:
-            common+=[i]
+            common += [i]
     return common
