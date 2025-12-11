528b0ba4e303094711843c68c0e9c69f09d1bec5
E <emilakbarov857@gmail.com>
2025-12-11 14:14:43 +0400

    dsad

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    nl = []
+    new_list = []
     for i in range(0, list_length):
         try:
-            div = my_list_1 / my_list_2
+            div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             div = 0
@@ -15,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             div = 0
         finally:
-            nl.append(div)
-    return nl
+            new_list.append(div)
+    return (new_list)
