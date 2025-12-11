7f7fb310dc6f5c5b620af185603bef68d35bd691
E <emilakbarov857@gmail.com>
2025-12-11 14:06:07 +0400

    dsad

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     n=0
     for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]),end='')
+        try :
+            print("{:d}".format(my_list[i]), end='')
             n += 1
         except (TypeError, ValueError):
             continue
