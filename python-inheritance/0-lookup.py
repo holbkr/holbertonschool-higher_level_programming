1cb30670fec8b83e3b9605e7568c10b898ad90a9
E <emilakbarov857@gmail.com>
2025-12-26 18:59:48 +0400

    fds

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -7,5 +7,5 @@ available attributes and methods of an object
 
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """Returns the list of available attributes and methods."""
     return dir(obj)
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,6 +8,6 @@ Contains function that compares an object with an instance.
 def is_same_class(obj, a_class):
     """
     Returns True if the object is exactly an instance
-    of the specified class ; otherwise False.
+    of the specified class; otherwise False.
     """
     return type(obj) is a_class
